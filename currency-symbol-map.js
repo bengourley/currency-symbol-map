2c96c2849651267d5571f7907d2a69ebf1e64d88
André Santos <acoelhosantos@gmail.com>
2018-06-29 09:56:06 +0100

    Improved code formatting: semicolons, spacing, indentation.

--- currency-symbol-map.js
+++ currency-symbol-map.js
@@ -1,10 +1,10 @@
-var currencySymbolMap = require('./map')
+var currencySymbolMap = require('./map');
 
-module.exports = function getSymbolFromCurrency (currencyCode) {
-  if (typeof currencyCode !== 'string') return undefined
-  var code = currencyCode.toUpperCase()
-  if (!currencySymbolMap.hasOwnProperty(code)) return undefined
-  return currencySymbolMap[code]
-}
+module.exports = function getSymbolFromCurrency(currencyCode) {
+  if (typeof currencyCode !== 'string') return undefined;
+  var code = currencyCode.toUpperCase();
+  if (!currencySymbolMap.hasOwnProperty(code)) return undefined;
+  return currencySymbolMap[code];
+};
 
-module.exports.currencySymbolMap = currencySymbolMap
+module.exports.currencySymbolMap = currencySymbolMap;
